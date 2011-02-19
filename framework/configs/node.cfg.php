43e7592ae2d7808f31f61c3eb8165685cbb091d1
unknown <Administrator@.(none)>
2011-02-19 18:11:10 +0800

    modify some file

--- framework/configs/node.cfg.php
+++ framework/configs/node.cfg.php
@@ -1,5 +1,4 @@
 <?php
-$GLOBALS['node_cfg']['asdf']=array('host'=>'localhost','port'=>'3311','user'=>'admin','passwd'=>'kangle','db_user'=>'','db_passwd'=>'','state'=>'0','type'=>'0','win'=>'1','dev'=>'e:');
-$GLOBALS['node_cfg']['asdffff']=array('host'=>'localhost','port'=>'3311','user'=>'admin','passwd'=>'kangle','db_user'=>'','db_passwd'=>'','state'=>'0','type'=>'0','win'=>'1','dev'=>'d:');
+$GLOBALS['node_cfg']['asdf']=array('host'=>'192.168.1.88','port'=>'3311','user'=>'admin','passwd'=>'kangle','db_user'=>'root','db_passwd'=>'test','state'=>'0','type'=>'0','win'=>'1','dev'=>'e:');
 $GLOBALS['node_cfg']['vip2']=array('host'=>'192.168.1.201','port'=>'3311','user'=>'admin','passwd'=>'kangle','db_user'=>'','db_passwd'=>'','state'=>'0','type'=>'0','win'=>'1','dev'=>'c:');
 ?>
