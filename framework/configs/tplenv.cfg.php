b25a3498a10aba4480426ce6aa929e0d7478ef41
wzy <wzy@example.com>
2011-03-18 15:17:47 +0800

    modify tomcat

--- framework/configs/tplenv.cfg.php
+++ framework/configs/tplenv.cfg.php
@@ -1,7 +1,7 @@
 <?php
 //$GLOBALS['tplenv']['templete[:subtemplete]'] = array('name'=>'env name','distinct'=>'0|1','password'=>'1',value='value_regex');
-$GLOBALS['tplenv']['php'][] = array('name'=>'TOMCAT_PASSWD','password'=>1,'count'=>1,'value'=>array('TEXT','/^[a-z0-9A-Z_]{2,16}$/'));
-//$GLOBALS['tplenv']['php'][] = array('name'=>'ZEND','count'=>1,'value'=>array('RADIO',array(array('ON',''),array('OFF',';'))));
+$GLOBALS['tplenv']['html'][] = array('name'=>'TOMCAT_PASSWD','password'=>1,'count'=>1,'value'=>array('TEXT','/^[a-z0-9A-Z_]{2,16}$/'));
+$GLOBALS['tplenv']['html'][] = array('name'=>'ZEND','count'=>1,'value'=>array('RADIO',array(array('ON',''),array('OFF',';'))));
 //$GLOBALS['tplenv']['php:php52'][] = array('name'=>'ZEND2','count'=>1,'value'=>array('CHECKBOX','',';'));
 //$GLOBALS['tplenv']['php'] = array('name'=>'ZEND','count'=>1,'value'=>array('SELECT',array('0','1','2')));
 ?>
