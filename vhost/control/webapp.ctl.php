dd9619856e9c4649d6854b08ba39f03f0facca86
keengo <keengo99@gmail.com>
2011-05-03 16:11:54 +0800

    install

--- vhost/control/webapp.ctl.php
+++ vhost/control/webapp.ctl.php
@@ -10,5 +10,28 @@ class WebappControl extends Control
 		$this->_tpl->assign('list',$list);		
 		return $this->_tpl->fetch('webapp/show.html');
 	}
+	public function browse()
+	{
+		return $this->install();
+	}
+	public function install()
+	{
+		$step = intval($_REQUEST['step']);
+	
+		if($step==0){
+			$this->_tpl->assign('appid','KKK1.zip');
+			$this->_tpl->assign('appname','phpwind');
+			$this->_tpl->assign('appver','8.5');
+			if($GLOBALS['free_domain']!=""){
+				$this->_tpl->assign('free_domain',getRole('vhost').'.'.$GLOBALS['free_domain']);
+			}	
+			$domain = daocall('vhostinfo', 'getDomain', array(getRole('vhost')));
+			return $this->_tpl->fetch('webapp/step0.html');
+		}
+	}
+	public function uninstall()
+	{
+		
+	}
 }
 ?>
