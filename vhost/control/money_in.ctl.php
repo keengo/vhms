681442d91f02d611d97f1c1b4a3b61f7e1577fc1
kuj <security621@126.com>
2011-05-12 09:28:00 +0800

    add rechargeFrom.html

--- vhost/control/money_in.ctl.php
+++ vhost/control/money_in.ctl.php
@@ -1,8 +1,8 @@
 <?php
-class Money_logControl extends Control {
+class Money_inControl extends Control {
 	
 	
-	public function pageMoney_logByUsername()
+	public function pageMoney_inByUsername()
 	{
 		$page = intval($_REQUEST['page']);
 		if($page<=0){
@@ -10,7 +10,7 @@ class Money_logControl extends Control {
 		}
 		$page_count = 12;
 		$count = 0;
-		$list = daocall('webapp','pageApp',array($appname,$file_exts,$page,$page_count,&$count));
+		$list = daocall('money_in','pageMoney_inByUsername',array($username,$page,$page_count,&$count));
 		$total_page = ceil($count/$page_count);
 		if($page>=$total_page){
 			$page = $total_page;
@@ -20,11 +20,14 @@ class Money_logControl extends Control {
 		$this->_tpl->assign('page',$page);
 		$this->_tpl->assign('page_count',$page_count);
 		$this->_tpl->assign('list',$list);
-		$this->_tpl->display('money_log/pagelist.html');
+		$this->_tpl->display('money_in/pagelist.html');
 	}
-	
+	public function addFrom()
+	{
+		$this->_tpl->display('money_in/rechargeFrom.html');
+	}
 	public function add()
 	{
-		$log=daocall('money_log','add',array($_REQUEST['username'],$_REQUEST['money'],$_REQUEST['gw'],$_REQUEST['gwid'],1));
+		$log=daocall('money_in','add',array($_REQUEST['username'],$_REQUEST['money'],$_REQUEST['gw'],$_REQUEST['gwid'],1));
 	}
 }
