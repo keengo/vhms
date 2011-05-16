1930c14e40ff29981c071b2b7987dff92e652dfc
unknown <keengo@.(none)>
2011-05-16 19:52:20 +0800

    update sql.

--- admin/control/kangle.sql
+++ admin/control/kangle.sql
@@ -152,3 +152,31 @@ CREATE TABLE IF NOT EXISTS `vhost_webapp` (
   KEY `user` (`user`,`appid`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=65 ;
 
+CREATE TABLE IF NOT EXISTS `setting` (
+  `name` varchar(255) NOT NULL,
+  `value` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `money_in` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(32) NOT NULL,
+  `money` int(11) NOT NULL DEFAULT '0',
+  `start_time` datetime NOT NULL,
+  `end_time` datetime DEFAULT NULL,
+  `gw` tinyint(4) NOT NULL DEFAULT '0',
+  `status` tinyint(4) NOT NULL DEFAULT '0',
+  `gwid` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `username` (`username`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='充值记录' AUTO_INCREMENT=9 ;
+
+CREATE TABLE IF NOT EXISTS `money_out` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(32) NOT NULL,
+  `money` int(11) NOT NULL,
+  `add_time` datetime NOT NULL,
+  `mem` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `username` (`username`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
--- admin/control/upgrade.sql
+++ admin/control/upgrade.sql
@@ -17,3 +17,32 @@ CREATE TABLE IF NOT EXISTS `vhost_webapp` (
 ALTER TABLE `vhost_product` ADD `domain` INT NOT NULL DEFAULT '-1',
 ADD `subtemplete` VARCHAR( 255 ) NULL DEFAULT NULL ,
 ADD `upid` INT NOT NULL DEFAULT '0';
+CREATE TABLE IF NOT EXISTS `setting` (
+  `name` varchar(255) NOT NULL,
+  `value` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `money_in` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(32) NOT NULL,
+  `money` int(11) NOT NULL DEFAULT '0',
+  `start_time` datetime NOT NULL,
+  `end_time` datetime DEFAULT NULL,
+  `gw` tinyint(4) NOT NULL DEFAULT '0',
+  `status` tinyint(4) NOT NULL DEFAULT '0',
+  `gwid` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `username` (`username`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='³äÖµ¼ÇÂ¼' AUTO_INCREMENT=9 ;
+
+
+CREATE TABLE IF NOT EXISTS `money_out` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(32) NOT NULL,
+  `money` int(11) NOT NULL,
+  `add_time` datetime NOT NULL,
+  `mem` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `username` (`username`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
