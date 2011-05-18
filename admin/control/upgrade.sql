05541293796cdaa8677db684ead28812c7d47316
keengo <keengo99@gmail.com>
2011-05-18 08:57:24 +0800

    adjust left menu.

--- admin/control/upgrade.sql
+++ admin/control/upgrade.sql
@@ -61,3 +61,11 @@ CREATE TABLE IF NOT EXISTS `question` (
   KEY `username` (`username`),
   KEY `status` (`status`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=22 ;
+
+CREATE TABLE IF NOT EXISTS `news` (
+  `id` int(11) NOT NULL auto_increment,
+  `title` varchar(255) NOT NULL,
+  `body` text NOT NULL,
+  `add_time` datetime NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
