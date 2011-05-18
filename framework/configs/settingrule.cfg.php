0582764c23beac44adc6010980b8005e5b372b4d
kuj <security621@126.com>
2011-05-18 15:00:59 +0800

    dfsf

--- framework/configs/settingrule.cfg.php
+++ framework/configs/settingrule.cfg.php
@@ -1,11 +1,11 @@
 <?php
-$GLOBALS['settingrule']['alipay']['ALIPAY_SELLER_EMAIL'] = array('name'=>'ALIPAY_SELLER_EMAIL','value'=>array('TEXT','/^[a-z0-9A-Z_]{2,32}@[a-z0-9A-Z_]{2,32}.[a-z]{2,32}$/',32));
-$GLOBALS['settingrule']['alipay']['ALIPAY_KEY'] = array('name'=>'ALIPAY_KEY','password'=>1,'value'=>array('TEXT','/^[a-z0-9A-Z_]{2,64}$/',32));
-$GLOBALS['settingrule']['alipay']['ALIPAY_PARTNER'] = array('name'=>'ALIPAY_PARTNER','value'=>array('TEXT','/^2088[0-9]{12}$/',32));
-$GLOBALS['settingrule']['alipay']['ALIPAY_MAINNAME'] = array('name'=>'ALIPAY_MAINNAME','value'=>array('TEXT','/^[a-z0-9A-Z_]{2,64}$/',32));
+$GLOBALS['settingrule']['alipay']['ALIPAY_SELLER_EMAIL'] = array('name'=>'ALIPAY_SELLER_EMAIL','value'=>array('TEXT','/.*/',32));
+$GLOBALS['settingrule']['alipay']['ALIPAY_KEY'] = array('name'=>'ALIPAY_KEY','password'=>1,'value'=>array('TEXT','/.*/',32));
+$GLOBALS['settingrule']['alipay']['ALIPAY_PARTNER'] = array('name'=>'ALIPAY_PARTNER','value'=>array('TEXT','/.*/',32));
+$GLOBALS['settingrule']['alipay']['ALIPAY_MAINNAME'] = array('name'=>'ALIPAY_MAINNAME','value'=>array('TEXT','/.*/',32));
 
 
-$GLOBALS['lang']['zh_CN']['ALIPAY_SELLER_EMAIL'] = '支付宝账号(seller_email)';
+$GLOBALS['lang']['zh_CN']['ALIPAY_SELLER_EMAIL'] = '支付宝账号';
 $GLOBALS['lang']['zh_CN']['ALIPAY_KEY'] = '支付宝安全码(key),仅修改时输入';
 $GLOBALS['lang']['zh_CN']['ALIPAY_PARTNER'] = '合作身份者ID';
 $GLOBALS['lang']['zh_CN']['ALIPAY_MAINNAME'] = '收款方名称';
