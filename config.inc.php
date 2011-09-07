fd01d01bd64e660753dc7ac37078a9077005c69c
unknown <Administrator@.(none)>
2011-09-07 21:03:55 +0800

    aaa

--- config.inc.php
+++ config.inc.php
@@ -7,22 +7,12 @@ define('UC_DBNAME', 'ucenter');
 define('UC_DBCHARSET', 'gbk');
 define('UC_DBTABLEPRE', '`ucenter`.ucenter_');
 define('UC_DBCONNECT', '0');
-define('UC_KEY', 'efbe6lHRM+UhMP4+lvoO4J4QF2P6zlbvvtGTgpA');
+define('UC_KEY', '4c88OvKs30DJ23E+nIi1zVSqPGRcZc5RcCdmYc0');
 define('UC_API', 'http://127.0.0.1/uc_server');
 define('UC_CHARSET', 'gbk');
 define('UC_IP', '');
-define('UC_APPID', '5');
+define('UC_APPID', '6');
 define('UC_PPP', '20');
 
 
-
-$dbhost = 'localhost';		// 
-$dbuser = 'root';			// ��ݿ��û���
-$dbpw = 'test';			// ��ݿ�����
-$dbname = 'ucenter';			// ��ݿ���
-$pconnect = 0;				// ��ݿ�־����� 0=�ر�, 1=��
-$tablepre = '';   	//ucenter数据库表前缀
-$dbcharset = 'gbk';			// MySQL �ַ�, ��ѡ 'gbk', 'big5', 'utf8', 'latin1', ����Ϊ������̳�ַ��趨
-							//ͬ����¼ Cookie ����
-$cookiedomain = ''; 		// cookie ������
-$cookiepath = '/';			// cookie ����·��
+$tablepre="";//DZ数据库表前缀
