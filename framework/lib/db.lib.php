a92426baabff1e79b8eca5fcb5cfd58b4655cfea
unknown <Administrator@.(none)>
2011-11-23 18:30:11 +0800

    del tiggerror

--- framework/lib/db.lib.php
+++ framework/lib/db.lib.php
@@ -55,7 +55,7 @@ function db_query(PDO $db,$sql, $ret_type = 'result')
 	}	
 	if(!$result && $db->errorCode()!='00000')
 	{
-		trigger_error('在MYSQL服务器端执行SQL语句失败.<br />SQL: ' . $sql . '<br />原因: '. $db->errorCode() . '<br />');
+		//trigger_error('在MYSQL服务器端执行SQL语句失败.<br />SQL: ' . $sql . '<br />原因: '. $db->errorCode() . '<br />');
 		return false;
 	}
 	switch($ret_type)
