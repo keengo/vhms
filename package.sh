e01c38695b1b1b5972973f4440db018c6e6d858e
root <root@centos55-x64.localdomain>
2011-12-05 09:54:19 +0800

    aa

--- package.sh
+++ package.sh
@@ -2,12 +2,12 @@
 git pull
 VERSION=`cat framework/runtime.php |grep VHMS_VERSION|grep -E '[.0-9]+' -o -m 1`
 rm -rf /tmp/vhms-$VERSION
-rm -f /tmp/vhms-$VERSION.zip
+rm -f /tmp/kangle-vhms-$VERSION.zip
 mkdir /tmp/vhms-$VERSION/upload -p
 cp * /tmp/vhms-$VERSION/upload -ar
 rm -rf /tmp/vhms-$VERSION/upload/readme
 rm -f /tmp/vhms-$VERSION/upload/*.sh
 cp readme /tmp/vhms-$VERSION -ar
-zip -qr /tmp/vhms-$VERSION.zip /tmp/vhms-$VERSION
-rm -rf /tmp/vhms-$VERSION
-echo "success package to zip file : /tmp/vhms-$VERSION.zip"
+cd /tmp/
+zip -qr kangle-vhms-$VERSION.zip vhms-$VERSION
+echo "success package to zip file : /tmp/kangle-vhms-$VERSION.zip"
