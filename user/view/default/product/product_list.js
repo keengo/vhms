b294379add099356eaff0077e83a2a7be7a1e592
unknown <user@.(none)>
2012-04-07 12:21:54 +0800

    fix productl_list.js

--- user/view/default/product/product_list.js
+++ user/view/default/product/product_list.js
@@ -1,18 +1,22 @@
-function go_product_form(product,id)
+function go_product_form(product)
 {
 	if(product.substr(0,1)=='_'){
 		return;
 	}
+	
 	if(product.substr(0,1)=='*'){
+		var len = product.indexOf('_');
+		var id = product.substr(len+1);
 		{{$target}}.window.location='?c=mproductorder&a=addMproductorderFrom&mproduct_id='+id;
 	}else{
 		{{$target}}.window.location='?c=product&a=sellForm&product='+product;
 	}
 }
-//document.write('<select name="product" onChange=go_product_form(this.value)>');
-document.write('<select name="product" >');
+document.write('<select name="product" onChange=go_product_form(this.value)>');
+//document.write('<select name="product" >');
 document.write('<option value="_">--产品快速导航--</option>');
 {{foreach from=$products item=product}}
-document.write('<option value="{{$product.type}}_{{$product.id}}" onclick="go_product_form(this.value,{{$product.id}});">{{if $product.type!=''}}&nbsp;&nbsp;{{/if}}{{$product.name}}</option>');
+//document.write('<option value="{{$product.type}}_{{$product.id}}" onclick="go_product_form(this.value,{{$product.id}});">{{if $product.type!=''}}&nbsp;&nbsp;{{/if}}{{$product.name}}</option>');
+document.write('<option value="{{$product.type}}_{{$product.id}}">{{if $product.type!=''}}&nbsp;&nbsp;{{/if}}{{$product.name}}</option>');
 {{/foreach}}
 document.write('</select>');
